bb70757ee8290fc17151900ba88d173a9459789b
alanmccoll <mccoll.alan@gmail.com>
2021-11-16 13:34:41 -0500

    First Commit

---
+++ web/themes/custom/mccoll_sub/js/custom.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.mccoll_sub = {
+    attach: function(context, settings) {
+
+      // Custom code here
+
+    }
+  };
+
+})(jQuery, Drupal);
